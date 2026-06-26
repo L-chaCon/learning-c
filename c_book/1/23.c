d366d84c8e56e1d16100963d0813bbfee2730182
Octavio Montt <montt.octavio@gmail.com>
2026-06-26 17:26:37 +0100

    change to state machine

--- c_book/1/23.c
+++ c_book/1/23.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 
+#define OUT 0
+#define CHECKING_START 1
+#define CHECKING_END 2
+#define LINE_COMMENT 3
+#define MULTY_COMMENT 4
+
 int main() {
-  int c, write, checking, cLine, cMulty, checking2;
-  write = 1;
-  checking = checking2 = cLine = cMulty = 0;
+  int s, c;
+  s = OUT;
   while ((c = getchar()) != EOF) {
-    if (cLine) {
-      if (c == '\n') {
-        write = 1;
-        cLine = 0;
-      }
-    }
-    if (cMulty) {
-      if (checking2) {
-        if (c == '/') {
-          write = 1;
-        } else {
-          checking2 = 0;
-        }
-      }
-      if (c == '*') {
-        checking2 = 1;
-      }
-    }
-    if (checking) {
-    }
-    if (c == '/') {
-      if (checking) {
-        cLine = 1;
-        checking = 0;
-      }
-    } else {
-      checking = 1;
-      write = 0;
-    }
-    if (write) {
-      if (cMulty) {
-        cMulty = 0;
+    // start with checking start becasue if I do after is going to enter every
+    // time the character is '/'
+    if (s == CHECKING_START) {
+      if (c == '/') {
+        s = LINE_COMMENT;
+      } else if (c == '*') {
+        s = MULTY_COMMENT;
       } else {
-        putchar(c);
+        putchar('/');
+        s = OUT;
       }
+    }
+    if (s == OUT && c == '/') {
+      s = CHECKING_START;
+    }
+    // Will deside that in case a comment with line is remove a \n will still
+    // stay there, this is for cases like some code like thie `s=0 //comment`
+    if (s == LINE_COMMENT && c == '\n') {
+      s = OUT;
+    }
+    if (s == OUT) {
+      putchar(c);
+    }
+    // This needs to go after the out because I want to insert the character
+    // after the end, not the end one
+    if (s == CHECKING_END && c == '/') {
+      s = OUT;
+    }
+    if (s == MULTY_COMMENT && c == '*') {
+      s = CHECKING_END;
     }
   }
 }
